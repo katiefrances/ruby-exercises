f996510cac8060786ced4365ef9a9e8eeaf4d577
Katie Frances <katiebeeisdead@gmail.com>
2018-02-27 16:10:54 +1100

    Added code to exercises

--- 3-arrays.rb
+++ 3-arrays.rb
@@ -7,13 +7,21 @@ subscribers = [
 # Take a moment to examine subscribers.
 
 # Put the first subscriber's email on screen
-
+puts subscribers[0]
 # Put the second subscriber's email on screen
-
+puts subscribers[1]
 # Ask the user for another email address
 # Only push it to the the array if it contains an @ symbol
 
+puts "What is your email?"
+email = gets.chomp
+
+
+
+
+
 # Put the last subscriber's email address on screen
+
 
 # Ask the user for another email address
 # Only add it to the array if it isn't already in the list
